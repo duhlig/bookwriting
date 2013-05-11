9105d634cea7bfeddcb0c82c64dc49d4dc682aee
David Uhlig <david.uhlig@gmail.com>
2013-05-11 17:00:07 +0200

    first commit (again)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookwriting::Application.config.secret_token = '55a2cfb9d555be82cc13e350671b69a9502ebd4e16f63c68f79503e41e1e3d5c16244def3bb407d06d783d92be4a6d2950d30fe03d4b051f6205ea7926217830'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bookwriting::Application.config.session_store :cookie_store, key: '_bookwriting_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bookwriting::Application.config.session_store :active_record_store
