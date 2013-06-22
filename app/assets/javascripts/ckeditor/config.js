e914ab5c132b9dafaa76c4e6d7a9b995b5eedd2a
KatharinaB <kathafd5@freenet.de>
2013-06-22 13:35:26 +0200

    fixed visible html-tag in abstract (Book-List)
    additional remove buttons form abstract-ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -27,16 +27,13 @@ CKEDITOR.editorConfig = function( config ) {
 	];
 
     config.toolbar_bookAbstract = [
-        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Undo', 'Redo' ] },
         { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
-        { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-        { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
         { name: 'tools', items: [ 'Maximize' ] },
         { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
-        { name: 'others', items: [ '-' ] },
-        '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] }    ];
+        { name: 'others', items: [ '-' ] }
+    ];
 
     // taken from http://ckeditor.com/ckeditor_4.1rc/samples/plugins/toolbar/toolbar.html
     config.toolbar_bookContent = [
